861f8eb5bf8f6275b4806abe5a35616d9243bdcf
bobo <495399597@qq.com>
2015-10-25 01:49:08 +0800

    初始化项目

---
+++ XYZ美食/XYZ美食/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XYZ美食
+//
+//  Created by 朱波 on 15/10/25.
+//  Copyright © 2015年 朱波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
