fbf4b4d90d67cc1f5b2a3d40ec61e2c6d712a312
bobo <495399597@qq.com>
2015-10-25 11:17:52 +0800

    初始化项目

---
+++ 团购/团购/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  团购
+//
+//  Created by 朱波 on 15/10/25.
+//  Copyright © 2015年 朱波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
